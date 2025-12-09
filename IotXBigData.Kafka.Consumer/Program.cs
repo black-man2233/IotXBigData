67b8966e213fe4f64d15123b5883cb5fbaa95a85
kevin arch abuser <kevi3758@gmail.com>
2025-12-09 13:23:48 +0100

    more init

---
+++ IotXBigData.Kafka.Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using Confluent.Kafka;
+
+namespace Consumer;
+class Program
+{
+  static string url = "kafka:9092";
+    static void Main()
+    {
+        // var db = new LocalDatabase();
+
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = "192.168.39.55",
+            GroupId = "kevin-Group",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        using var consumer = new ConsumerBuilder<string, string>(config).Build();
+        consumer.Subscribe("Kevin_Zilas_Mathias");
+
+        Console.WriteLine("Consumer running...");
+
+        while (true)
+        {
+            var cr = consumer.Consume();
+            Console.WriteLine($"Received: {cr.Message.Value}");
+        }
+    }
+}
